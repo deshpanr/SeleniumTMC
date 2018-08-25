da1efb2703f12159c1ab361a062cb5fc64aba5af
deshpanr <42548495+deshpanr@users.noreply.github.com>
2018-08-25 12:11:32 +0530

    Update TestSourceUIFunctionality.java
    Updated TMCSource URL

--- src/co/testNG/tmcsourceui/TestSourceUIFunctionality.java
+++ src/co/testNG/tmcsourceui/TestSourceUIFunctionality.java
@@ -47,7 +47,7 @@ public class TestSourceUIFunctionality {
 					driver.manage().window().maximize();
 					driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 					driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
-					driver.get("http://localhost:8080/tmc-source-ws/html");
+					driver.get("http://localhost:8085/tmc-source-ws/html");
 					
 				} catch (Exception e) {
 					e.printStackTrace();
