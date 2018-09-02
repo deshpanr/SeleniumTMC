3087f2fa668b8bc785e897aba712346b67c93303
deshpanr <42548495+deshpanr@users.noreply.github.com>
2018-09-02 18:41:35 +0530

    updated assertion

--- src/co/testNG/tmcsourceui/TMCSourceUILogin.java
+++ src/co/testNG/tmcsourceui/TMCSourceUILogin.java
@@ -50,7 +50,7 @@ public class TMCSourceUILogin {
 		if(isAlertPresent()){
 		driver.switchTo().alert().accept();
 		boolean Upload=driver.findElement(By.xpath("//form[@id='upload-form']/button[1]/span[@class='glyphicon glyphicon-upload']")).isDisplayed();
-		Assert.assertFalse(Upload, "Invalid username and password");
+		Assert.assertTrue(Upload, "Inavlid username and password");
 			}
 			else{
 				boolean Upload=driver.findElement(By.xpath("//form[@id='upload-form']/button[1]/span[@class='glyphicon glyphicon-upload']")).isDisplayed();
